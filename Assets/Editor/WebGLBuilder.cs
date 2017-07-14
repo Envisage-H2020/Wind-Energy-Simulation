b138d355474112856098f1911a62b8b377cda74b
jimver <jimver04@gmail.com>
2017-07-14 08:08:32 +0300

    Enabled build for WebGL from command line

---
+++ Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,24 @@
+﻿//place this script in the Editor folder within Assets.
+using UnityEditor;
+
+
+//to be used on the command line:
+//$ Unity -quit -batchmode -executeMethod WebGLBuilder.build
+
+class WebGLBuilder {
+	static void build() {
+		
+		string[] scenes = {"Assets/scenes/S_MainMenu.unity", 
+							"Assets/scenes/S_Login.unity",
+							"Assets/scenes/S_Help.unity",
+							"Assets/scenes/S_1.unity",
+							"Assets/scenes/S_Reward.unity",
+							"Assets/scenes/S_Credits.unity",
+							"Assets/scenes/S_Settings.unity",
+							"Assets/scenes/S_SceneSelector.unity"};
+		
+		string pathToDeploy = "builds/WebGLversion/"		
+				
+		BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);
+	}
+}
---
+++ compile_with_class.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -logFile stdout.txt -executeMethod WebGLBuilder.build
+pause
