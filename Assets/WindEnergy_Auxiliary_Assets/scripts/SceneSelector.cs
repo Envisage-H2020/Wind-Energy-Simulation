8ece399d931c748b29612752139fbb9eba55a433
jimver <jimver04@gmail.com>
2017-07-17 14:27:34 +0300

    Settings and SceneSelector Fixes

--- Assets/WindEnergy_Auxiliary_Assets/scripts/SceneSelector.cs
+++ Assets/WindEnergy_Auxiliary_Assets/scripts/SceneSelector.cs
@@ -16,13 +16,11 @@ public class SceneSelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 	void Start () {
 		rt = gameObject.GetComponent<RectTransform>();
 		sh = gameObject.GetComponent<Shadow> ();
-
-		bt_scene = gameObject.transform.Find ("bt_scene").GetComponent<Button>();
-		txt_bt_scene_title = gameObject.transform.Find ("bt_scene").gameObject.transform.Find ("txt_bt_scene_title").GetComponent<Text>();
+			
+		bt_scene 		         = gameObject.transform.Find ("bt_scene").GetComponent<Button>();
+		txt_bt_scene_title       = gameObject.transform.Find ("bt_scene").gameObject.transform.Find ("txt_bt_scene_title").GetComponent<Text>();
 		txt_bt_scene_short_descr = gameObject.transform.Find ("bt_scene").gameObject.transform.Find ("txt_bt_scene_short_descr").GetComponent<Text>();
-
 	}
-
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
@@ -30,9 +28,7 @@ public class SceneSelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 		bt_scene.GetComponent<Image> ().color = Color.white;
 		txt_bt_scene_title.color = Color.black;
 		txt_bt_scene_short_descr.color = Color.black;
-
 		sh.effectColor = new Color32(127/255,127/255,127/255,127);
-
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
