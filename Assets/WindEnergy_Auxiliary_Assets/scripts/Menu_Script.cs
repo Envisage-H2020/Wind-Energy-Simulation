f769055b322d5213b6c5b444aa0c6f80779f65fc
jimver <jimver04@gmail.com>
2017-07-17 11:18:51 +0300

    SceneSelector Tab Resize on Mouse Enter

--- Assets/WindEnergy_Auxiliary_Assets/scripts/Menu_Script.cs
+++ Assets/WindEnergy_Auxiliary_Assets/scripts/Menu_Script.cs
@@ -77,4 +77,6 @@ public class Menu_Script : MonoBehaviour {
 	}
 
 
+
+
 }
---
+++ Assets/WindEnergy_Auxiliary_Assets/scripts/SceneSelector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SceneSelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	RectTransform rt;
+	Button bt_scene;
+	Text txt_bt_scene_title, txt_bt_scene_short_descr;
+	Shadow sh;
+
+	// Use this for initialization
+	void Start () {
+		rt = gameObject.GetComponent<RectTransform>();
+		sh = gameObject.GetComponent<Shadow> ();
+
+		bt_scene = gameObject.transform.Find ("bt_scene").GetComponent<Button>();
+		txt_bt_scene_title = gameObject.transform.Find ("bt_scene").gameObject.transform.Find ("txt_bt_scene_title").GetComponent<Text>();
+		txt_bt_scene_short_descr = gameObject.transform.Find ("bt_scene").gameObject.transform.Find ("txt_bt_scene_short_descr").GetComponent<Text>();
+
+	}
+
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		rt.localScale = new Vector3 (1.1f, 1.1f, 1); 
+		bt_scene.GetComponent<Image> ().color = Color.white;
+		txt_bt_scene_title.color = Color.black;
+		txt_bt_scene_short_descr.color = Color.black;
+
+		sh.effectColor = new Color32(127/255,127/255,127/255,127);
+
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		rt.localScale = new Vector3 (1, 1, 1); 
+		bt_scene.GetComponent<Image> ().color = new Color32(0,0,0,127);
+		txt_bt_scene_title.color = Color.white;
+		txt_bt_scene_short_descr.color = Color.white;
+		sh.effectColor = new Color32(255/255,255/255,255/255,0);
+	}
+}
